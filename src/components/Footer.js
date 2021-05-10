900643d6be2a5dbc6d1f110b438733f52e451039
Brian Hensley <brnhensley@gmail.com>
2021-05-10 10:35:15 -0700

    clone webapp to convert to react native

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import '../css/App.css';
+
+export default function Footer() {
+
+    return (
+        <footer className="App" id="footer">
+            <p>© 2021 Brian Hensley</p>
+        </footer>
+    );
+}
